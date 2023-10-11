7d22ef02e1fd508e9c2cc7ed8344804caa933742
seasons986 <annalin12345@gmail.com>
2023-10-10 22:13:36 -0400

    init

---
+++ README.md
@@ -0,0 +1,53 @@
+# Coding Lab Discord Bot
+
+The Coding Lab Discord Bot is a bot that helps ITP/IMA students find information about the coding lab resources for virtual help on the Discord platform.
+
+![Logo](assets/codinglab-logo.png)
+
+## About
+
+The Coding Lab Discord bot is a bot that gives information about ITP/IMA's coding lab. It allows users on the ITP/IMA Discord server to query the Coding Lab's Google Sheet to provide up-to-date information about mentors, skills, timeslots, and how to get remote hours. 
+
+## Usage
+```
+!codinglab - provides a list of coding lab mentors, their skills, hours, and contact info
+!codinglab skill <any skill> - provides a list of mentors that know the requested skill
+!codinglab help - provides help guide
+```
+
+## Setup
+
+### Prerequisites
+
+To run the discord bot you must have:
+1. Node.js
+2. Membership to the ITP Discord Server
+3. Authentication files:
+  1. auth.json
+  2. `.env` for channel authentication
+4. Update the mentors.json file
+
+### Installation
+
+1. Clone the repository
+2. Change to the directory: `cd codinglab-bot`
+3. Install necessary node packages with: `npm install`
+4. Save the authentication files to the `codinglab-bot` directory
+5. Run the bot with `npm start`
+
+### Develop & Contribute
+
+Please submit bugs and feature requests as github issues.  
+Please e-mail the project owner to get the authentication files.  
+Feel free to fork the project and submit a pull request
+
+## Built with
+
+* [VIM](https://www.vim.org/)
+* [Github](https://github.com)
+
+## Authors
+
+* [Mark Lam](https://markofthelam.com) 
+* [Vince Shao](https://www.vinceshao.com/) 
+* [Billy Bennett](https://github.com/billythemusical)
---
+++ bot.js
@@ -0,0 +1,109 @@
+require("dotenv").config()
+let channelID = process.env.CHANNEL_ID;
+console.log(`Working with the Channel ID: ${channelID}`);
+
+var Discord = require("discord.js");
+var logger = require("winston");
+var auth = require("./secret/discord.json");
+const cron = require("node-cron");
+
+var fs = require('fs');
+var mentorData = Array.from(JSON.parse(fs.readFileSync('mentors.json', 'utf8')));
+const codingLabSite = "https://itp.nyu.edu/help/in-person-help/coding-lab";
+
+// Configure logger settings
+logger.remove(logger.transports.Console);
+logger.add(new logger.transports.Console(), {
+  colorize: true
+});
+logger.level = "debug";
+
+// Initialize Discord Bot
+var bot = new Discord.Client();
+
+bot.login(auth.token);
+
+bot.on("ready", () => {
+  logger.info("Logged in as: " + bot.user.tag);
+});
+
+bot.on("message", (message) => {
+  // ignore if message was sent by a bot
+  if (message.author.bot) return;
+  // console.log(message)
+  // Our bot needs to know if it will execute a command
+  // It will listen for messages that will start with `!`
+  if (message.channel.id == channelID && message.content.substring(0, 1) == "!") {
+    var args = message.content.substring(1).split(" ");
+    var cmd = args[0];
+    args = args.splice(1);
+    switch (cmd) {
+      // !ping
+      case "codinglab":
+        const subCmd = args[0];
+        const embed = new Discord.MessageEmbed()
+          .setColor('#39FF14');
+        switch (subCmd) {
+          case "help":
+            embed.setTitle(`Help!`);
+            embed.addField('\u200b', "To find a lab tech with a particular skill, use: \n`!codinglab skill <skill i.e. 'javascript'>` \nor you can list all lab techs with: \n`!codinglab`");
+            bot.channels.cache.get(channelID).send(embed);
+            break;
+          case "skill":
+            if (!args[1]) return;
+            const skill = args[1].toLowerCase().trim();
+            embed.setTitle(`Help with ${skill}`);
+
+            const matches = mentorData.filter((mentor) => mentor.skills.includes(skill));
+            matches.forEach((match) => {
+              embed.addField(`${match.name} can help!`, `${match.name}'s office hours are ${match.time}.\nBook here: `, `${match.link}.`);
+            });
+            if (matches.length < 1) {
+              embed.addField('\u200b', `No particular experts here :(`);
+            }
+            embed.addField('\u200b', `Book a mentor or find a mentor on the coding lab site (${codingLabSite})`);
+            bot.channels.cache.get(channelID).send(embed);
+            break;
+          default:
+            embed.setTitle(`All Coding Lab mentors`);
+            
+            mentorData.forEach((m) => {
+              embed.addField(`${m.name}`, `${m.name}'s office hours are ${m.time}!`);
+            });
+            embed.addField('\u200b', `Book a mentor or find a mentor on the coding lab site (${codingLabSite})`);
+            bot.channels.cache.get(channelID).send(embed);
+            break;
+        }
+        break;
+      // Just add any case commands if you want to..
+    }
+  }
+});
+
+const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+
+// Cron does time like so:
+// ('<minutes(of 60)> <hours(of 24)> <days(of month)> <months> <year>')
+// * means "every"
+cron.schedule('0 8 * * *', function (err) {
+    if (err) {
+      console.log('Cron Job - There was an error ' + error);
+    }
+
+    const day = new Date().getDay(); //returns 0-6 for Sun-Sat
+    const dayName = days[day];
+
+    const embed = new Discord.MessageEmbed()
+              .setColor('#39FF14')
+              .setTitle(`Help with ${skill}`);
+            
+    const matches = mentorData.filter((mentor) => mentor.time.includes(dayName));
+    matches.forEach((match) => {
+      embed.addField(`${match.name} is in the lab ${match.time}!`, `Feel free to drop by or book them here: ${match.link}`);
+      embed.addField('\u200b', `If you missed us, you can always make an appointment at ${codingLabSite}`);
+    });
+    bot.channels.cache.get(channelID).send(embed);
+  },
+{
+  scheduled: true, timezone: "America/New_York"
+});
